50bfa2816b8219a8c00c69f9e884da90963fffa9
marcuslin-eslite <marcuslin@eslite.com>
2021-07-11 00:26:35 +0800

    Add users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,35 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :check_edit_permission
+
   def edit
-    @form = UpdateUserForm.new(User.find_by(id: params[:id]))
+    form
+  end
+
+  def update
+    if form.validate(params[:user])
+      flash[:notice] = 'Updated successfully'
+
+      form.save
+    else
+      flash[:error] = form.errors.full_messages
+    end
+
+    redirect_to edit_user_path(form.model)
+  end
+
+  private
+
+  def form
+    @form ||= UpdateUserForm.new(User.find_by(id: params[:id]))
+  end
+
+  def check_edit_permission
+    if current_user != form.model
+      flash[:error] = 'You are not allowed to perform this action'
+
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
