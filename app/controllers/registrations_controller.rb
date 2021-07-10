a9b5c3023d5b733d866f7cf654d6f8431bc466e4
marcuslin-eslite <marcuslin@eslite.com>
2021-07-10 23:28:36 +0800

    Add registrations#create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,21 @@ class RegistrationsController < ApplicationController
   def new
     @form = RegistrationForm.new(User.new)
   end
+
+  def create
+    @form = RegistrationForm.new(User.new)
+
+    if @form.validate(params[:user])
+      @form.save
+      user = @form.sync
+
+      WelcomeMailer.send_welcome_mail(user).deliver_now
+
+      sign_in(user, 'Sign up successfully')
+    else
+      flash[:error] = @form.errors.full_messages
+
+      render(:new)
+    end
+  end
 end
