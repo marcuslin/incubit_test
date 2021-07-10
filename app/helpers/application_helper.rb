4e48db462da77b75ad3291a43ce49c3338d9275f
marcuslin-eslite <marcuslin@eslite.com>
2021-07-10 22:58:29 +0800

    Add flash_messages in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_messages
+    messages = flash[:notice] || flash[:error]
+    flash_messageg_classes = { notice: 'success', error: 'danger' }
+
+    if flash.any?
+      tag.div(class: "alert alert-#{flash_messageg_classes[flash.keys.first.to_sym]}") do
+        if messages.is_a?(String)
+          tag.li(messages)
+        else
+          messages.collect { |message| concat(tag.li(message)) }
+        end
+      end
+    end
+  end
 end
