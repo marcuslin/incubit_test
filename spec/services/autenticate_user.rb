a59b8d364a584d502b4df5375cc87a6f82a1e742
marcuslin-eslite <marcuslin@eslite.com>
2021-07-11 01:49:08 +0800

    Add tests for services/authenticate_user

---
+++ spec/services/autenticate_user.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+module Sessions
+  RSpec.describe Authenticate do
+    let(:user) { create(:user) }
+    let(:locked_user) { create(:locked_user) }
+    let(:expire_time) { ENV['VALID_RESET_TOKEN_DURATION'].to_i.hours.since }
+    let(:delivered) { ActionMailer::Base.deliveries }
+    let(:user_params) do
+      {
+        email: user.email,
+        password: user.password
+      }
+    end
+
+    let(:subject) { described_class.call(user, user_params) }
+
+    describe '#call' do
+      it 'return itself' do
+        expect(subject.class).to eq described_class
+      end
+    end
+
+    describe '#success?' do
+      context 'with correct sign in information' do
+        it 'return true' do
+          expect(subject.success?).to be_truthy
+        end
+      end
+
+      context 'with incorrect password' do
+        let(:user_params) do
+          {
+            email: user.email,
+            password: Faker::Internet.password
+          }
+        end
+
+        it 'return false' do
+          expect(subject.success?).to be_falsey
+        end
+      end
+
+      context 'with incorrect email' do
+        let(:user_params) do
+          {
+            email: Faker::Internet.email,
+            password: Faker::Internet.password
+          }
+        end
+
+        it 'return false' do
+          expect(subject.success?).to be_falsey
+        end
+      end
+
+      context 'with locked user' do
+        let(:user_params) do
+          {
+            email: locked_user.email,
+            password: locked_user.password
+          }
+        end
+        let(:subject) { described_class.call(locked_user, user_params) }
+
+        it 'return false' do
+          expect(subject.success?).to be_falsey
+        end
+      end
+    end
+
+    describe '#error_message' do
+      context 'with correct sign in information' do
+        it 'return true' do
+          expect(subject.error_message).to be_nil
+        end
+      end
+
+      context 'with incorrect password' do
+        let(:user_params) do
+          {
+            email: user.email,
+            password: Faker::Internet.password
+          }
+        end
+
+        it 'return false' do
+          expect(subject.error_message).to eq('The email or password you entered is incorrect')
+        end
+      end
+
+      context 'with incorrect email' do
+        let(:user_params) do
+          {
+            email: Faker::Internet.email,
+            password: Faker::Internet.password
+          }
+        end
+
+        it 'return false' do
+          expect(subject.error_message).to eq('The email or password you entered is incorrect')
+        end
+      end
+
+      context 'with locked user' do
+        let(:user_params) do
+          {
+            email: locked_user.email,
+            password: locked_user.password
+          }
+        end
+        let(:subject) { described_class.call(locked_user, user_params) }
+
+        it 'return false' do
+          expect(subject.error_message).to eq('This account has been locked, please contact administrator for unlocking account')
+        end
+      end
+    end
+  end
+end
