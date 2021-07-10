c88de1605257014af1e1d1a7e884ec841b27af9b
marcuslin-eslite <marcuslin@eslite.com>
2021-07-11 01:18:05 +0800

    Add sessions#new, create, destroy

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new
+    redirect_to edit_user_path(current_user.id) if current_user
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    user_authentication = Sessions::AuthenticateUser.call(@user, params[:user])
+
+    if user_authentication.authorized?
+      sign_in(@user)
+    else
+      flash[:error] = user_authentication.error_message
+
+      Sessions::LockUserService.call(@user) if @user
+
+      render(:new)
+    end
+  end
+
+  def destroy
+    session[:user] = nil if current_user
+
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'registrations#new'
+  root 'sessions#new'
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy'
 
   resources :users, only: %i[edit update]
 end
