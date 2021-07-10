ec0cccbd0e1275fa03ee29e5874b33ce61aa2917
marcuslin-eslite <marcuslin@eslite.com>
2021-07-10 23:01:58 +0800

    Add current_user, sign_in to ApplicatoinController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
+
+  def sign_in(user, notification_message = 'Signed in')
+    session[:user] = user.id
+
+    redirect_to edit_user_path(user), notice: notification_message
+  end
 end
